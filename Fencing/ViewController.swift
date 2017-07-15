cf3083945e83561ee6019f36984d9061a6315406
Jeff Ripke <jripke74@gmail.com>
2017-07-15 07:39:12 -0700

    add push and pull

--- Fencing/ViewController.swift
+++ Fencing/ViewController.swift
@@ -12,23 +12,55 @@ import CoreMotion
 class ViewController: UIViewController {
     
     let motionManager = CMMotionManager()
+    let interval = 0.5
+    var timer = Timer()
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if isDeviceAvailable() {
             print("Jeff: Core Motion Launched")
+            myDeviceMotion()
         }
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        motionManager.stopDeviceMotionUpdates()
+        timer.invalidate()
+    }
     func isDeviceAvailable() -> Bool {
-        let gyroAvailable = motionManager.isGyroAvailable
-        let accelAvailable = motionManager.isAccelerometerAvailable
+//        let gyroAvailable = motionManager.isGyroAvailable
+//        let accelAvailable = motionManager.isAccelerometerAvailable
         if !motionManager.isAccelerometerAvailable {
             let alert = UIAlertController(title: "Fencing", message: "Your device does not have the necessary sensors. You migt want to try another device", preferredStyle: .alert)
             present(alert, animated: true, completion: nil)
-            print("JEFF: Devices not detected")
+            print("JEFF: Device not detected")
         }
         return motionManager.isDeviceMotionAvailable
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true, block: { (timer) in
+            if let deviceMotion = self.motionManager.deviceMotion {
+                print(String(format: "X: %7.4f", deviceMotion.userAcceleration.x))
+            }
+        })
+    }
+    
+    func myDeviceMotion() {
+        motionManager.deviceMotionUpdateInterval = interval
+        // pull
+        motionManager.startDeviceMotionUpdates()
+        startTimer()
+        /*
+        // normally don't use main for motion
+        // push
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (deviceMotion, error) in
+            if let deviceMotion = deviceMotion {
+                print("JEFF: \(deviceMotion.userAcceleration.x) \(Date())")
+            }
+        }
+        */
     }
 }
 
